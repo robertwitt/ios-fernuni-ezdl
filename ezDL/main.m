7986eb65bea46ba8a030fd1bc78355f70a90c233
Robert Witt <robert@Robert-Witts-iMac.local>
2011-11-18 14:10:14 +0100

    Initial Commit

---
+++ ezDL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ezDL
+//
+//  Created by Robert Witt on 18.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
